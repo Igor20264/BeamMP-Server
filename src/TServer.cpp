b7c8860e4c1dce30e24b9cf6ce874251c10143d2
SaltySnail <julianstaphorsius@hotmail.com>
2024-06-13 23:00:52 +0200

    fix all other places where onVehicleDeleted isn't triggered after a delete packet is sent

--- src/TServer.cpp
+++ src/TServer.cpp
@@ -333,6 +333,7 @@ void TServer::ParseVehicle(TClient& c, const std::string& Pckt, TNetwork& Networ
                     // TODO: handle
                 }
                 std::string Destroy = "Od:" + std::to_string(c.GetID()) + "-" + std::to_string(CarID);
+                //LuaAPI::MP::Engine->ReportErrors(LuaAPI::MP::Engine->TriggerEvent("onVehicleDeleted", "", c.GetID(), CarID)); //uncomment if this packet is sent to everyone instead of to the client that spawned too many vehicles
                 if (!Network.Respond(c, StringToVector(Destroy), true)) {
                     // TODO: handle
                 }
@@ -366,6 +367,7 @@ void TServer::ParseVehicle(TClient& c, const std::string& Pckt, TNetwork& Networ
                 }
                 std::string Destroy = "Od:" + std::to_string(c.GetID()) + "-" + std::to_string(VID);
                 Network.SendToAll(nullptr, StringToVector(Destroy), true, true);
+                LuaAPI::MP::Engine->ReportErrors(LuaAPI::MP::Engine->TriggerEvent("onVehicleDeleted", "", c.GetID(), VID));
                 c.DeleteCar(VID);
             }
         }
